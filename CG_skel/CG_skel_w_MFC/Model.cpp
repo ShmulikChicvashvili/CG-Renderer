a3dcf9067623c1e9449db08351f3b564c64b754f
1ceeb7d1-22ad-4501-9744-fa931e77dffe <1ceeb7d1-22ad-4501-9744-fa931e77dffe>
2014-11-19 17:36:39 +0000

    empty virutal functions

--- CG_skel/CG_skel_w_MFC/Model.cpp
+++ CG_skel/CG_skel_w_MFC/Model.cpp
@@ -8,3 +8,19 @@ const mat4 Model::getModelMatrix() const{
 const vector<Face>& Model::getFaces() const{
 	return faces;
 }
+
+void Model::spin(const GLfloat teta, const Axes axis){
+
+}
+
+void Model::scale(const GLfloat sx, const GLfloat sy, const GLfloat sz){
+
+}
+
+void Model::translate(const GLfloat tx, const GLfloat ty, const GLfloat tz){
+
+}
+
+void Model::rotate(const GLfloat teta, const Axes axis){
+
+}
